9b4b4f7ac29b0d8f9db5a4d2510483258a55ee1c
diki <sumplit>
2015-04-23 21:29:20 +0700

    second commit

--- prima.php
+++ prima.php
@@ -1,32 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
+<form action="" method="POST">
+	<p>Input Number(N)</p>
+	<input type="text" name="value">
+	<input type="submit" name="hitung" value="Show">
+</form>
 <?php
-$i=0;
-$limit=3;
-$start=0;
-$ii=2;
-$jj=0;
+if(isset($_POST['hitung'])){
+	$limit = trim($_POST['value']);
+	$k = 0;
+	$j1 = 2;
+	$j2 = 2;
+	
+	while($k <= $limit){
 
-while($start < $limit){
-	
-	for($i=2;$i<=$ii;$i++){
+		while($j2 <= $j1){
 		
-		$a = 0;
-		
-		for($j=1;$j<=$i;$j++){
-			if($i % $j == 0){
-				$a++;
+			$a=0;
+			for($i=1;$i<=$j1; $i++){		
+				if($j1 % $i == 0)
+					$a++;						
 			}
+			
+			if($a==2){
+				echo "N = ".$k." Result Prima = ".$j1."<br>";
+				$k++;
+				$j2++;
+				
+			}
+			$j2++;
+			$j1++;
 		}
 		
-		if($a == 2){
-			echo $start." -> ".$i.'<br/>';
-			$ii++;
-			$start++;
-			//$jj++;
+		if($k<=$limit){
+			$j2--;
 		}
-		
 	}
-	
-	//echo $ii."<br>";
-	//$start++;
-}//*/
+}
 ?>
+</body>
+</html>
